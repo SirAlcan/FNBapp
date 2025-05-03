1a5e032caff285e419e542963409554bf68239ba
SirAlcan <gregkazakou@gmail.com>
2025-05-03 14:37:16 +0300

    Section 12 Delete Action

---
+++ FNBapp/wwwroot/js/common.js
@@ -0,0 +1,24 @@
+﻿// Bootstrap modal functions
+window.ShowConfirmationModal = function () {
+    var modalElement = document.getElementById('bsConfirmationModal');
+    if (modalElement) {
+        var modal = new bootstrap.Modal(modalElement);
+        modal.show();
+    } else {
+        console.error("Modal element not found");
+    }
+};
+
+window.HideConfirmationModal = function () {
+    var modalElement = document.getElementById('bsConfirmationModal');
+    if (modalElement) {
+        var modal = bootstrap.Modal.getInstance(modalElement);
+        if (modal) {
+            modal.hide();
+        } else {
+            console.error("Modal instance not found");
+        }
+    } else {
+        console.error("Modal element not found");
+    }
+};
