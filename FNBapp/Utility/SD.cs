cd1704f36f3a6bd2144cbb5d900debc5a1f4c179
SirAlcan <gregkazakou@gmail.com>
2025-05-08 00:03:56 +0300

    Section 14 Register

---
+++ FNBapp/Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿namespace FNBapp.Utility
+{
+    public static class SD
+
+
+    {
+        public static string Role_Admin = "Admin";
+        public static string Role_Customer = "Customer";
+        public static string Role_Employee = "Employee";
+        public static string Role_StoreManager = "StoreManager";
+        public static string Role_Waiter = "Waiter";
+    }
+}
