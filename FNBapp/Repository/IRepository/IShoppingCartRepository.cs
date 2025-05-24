bee4010539393c1dfbccc8da3dbc2ccad093e90b
SirAlcan <gregkazakou@gmail.com>
2025-05-25 00:09:58 +0300

    Section 16: Shopping Cart

---
+++ FNBapp/Repository/IRepository/IShoppingCartRepository.cs
@@ -0,0 +1,11 @@
+﻿using FNBapp.Data;
+
+namespace FNBapp.Repository.IRepository
+{
+    public interface IShoppingCartRepository
+    {
+        public Task<bool> UpdateCartAsync(string userId, int productId, int updateBy);
+        public Task<IEnumerable<ShoppingCart>> GetAllAsync(string? userId);
+        public Task<bool> ClearCartAsync(string userId);
+    }
+}
